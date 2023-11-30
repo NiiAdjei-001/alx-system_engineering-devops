f0e89aa6ea6609dd7492c793f24a7e8a84bb8a1f
Nii Adjei Osae <niiadjeiosae.technology@gmail.com>
2023-11-30 02:37:11 +0000

    Scale update

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,5 @@
 # Execute a command to kill process with name killmenow
 exec { 'execute kill command':
-    command => 'pkill -9 killmenow'
+    command => 'pkill -9 "killmenow"',
+    path    => ['/usr/bin/']
 }
