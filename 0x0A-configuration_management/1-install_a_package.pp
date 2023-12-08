ce917645035b335c45f7fc8bd3d4df37eba5818b
Nii Adjei Osae <niiadjeiosae.technology@gmail.com>
2023-12-08 13:39:44 +0000

    task 1 correction

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -14,5 +14,5 @@ package { $python_packages:
 package { 'flask':
     ensure   => '2.1.0',
     provider => 'pip3',
-    require  => Package['python3-pip']
+    require  => Package[$python_packages]
 }
