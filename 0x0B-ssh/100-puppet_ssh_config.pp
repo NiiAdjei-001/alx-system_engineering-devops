c3e5779097aa95add750ef8ad8f7de040cbbc39c
Nii Adjei Osae <niiadjeiosae.technology@gmail.com>
2023-12-19 08:35:29 +0000

    Correction

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,8 +2,8 @@
 file { '/etc/ssh/ssh_config':
   ensure  => present,
   mode    => '0600',
-  content => "HOST 18.204.8.229
-    HostName 18.204.8.229
+  content => "HOST 100.26.235.117
+    HostName 100.26.235.117
     User ubuntu
     PasswordAuthentication no
     IdentityFile ~/.ssh/school\n"
