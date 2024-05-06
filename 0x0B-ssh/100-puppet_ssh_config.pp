2440ed25e2687e048e3dc0484db7f0880017d200
Nii Adjei Osae <niiadjeiosae.technology@gmail.com>
2024-05-06 05:37:32 +0000

    Changes to host ip

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,8 +2,8 @@
 file { '/etc/ssh/ssh_config':
   ensure  => present,
   mode    => '0600',
-  content => "HOST 100.26.235.117
-    HostName 100.26.235.117
+  content => "HOST 100.26.252.151
+    HostName 100.26.252.151
     User ubuntu
     PasswordAuthentication no
     IdentityFile ~/.ssh/school\n"
