2bd1ab0e3ccf586f4bcea2f36ff4657f870ac59b
Nii Adjei Osae <niiadjeiosae.technology@gmail.com>
2024-05-15 11:44:23 +0000

    0x0A Configuration Managment Task 1 Update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,18 +1,5 @@
 # Install Flask v2.1.0 package
-$python_packages = ['python3', 'python3-pip']
-
-exec { 'apt update':
-    command => '\usr\bin\apt update'
-}
-
-package { $python_packages:
-    ensure   => 'installed',
-    provider => 'apt',
-    require  => Exec['apt update']
-}
 
 package { 'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3',
-    require  => Package[$python_packages]
+    ensure  => '2.1.0'
 }
