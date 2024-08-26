292281872c2efcaefd215a15e5e782600fa9fc3a
Nii Adjei Osae <niiadjeiosae.technology@gmail.com>
2024-08-26 16:43:33 +0000

    Correction 3 on Task 0, 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,13 +11,20 @@ def number_of_subscribers(subreddit):
     """
     headers = {'User-Agent': 'ALX Client', }
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    
     response = requests.get(url, headers=headers, allow_redirects=False)
+    
     if response.status_code == 200:
-        return response.json().get('data').get('subscribers')
+        try:
+            data = response.json().get('data', {})
+            return data.get('subscribers', 0)
+        except (ValueError, KeyError):
+            return 0
         # return response.json()['data']['subscribers']
+    elif response.status_code == 404:
+        return 0
     else:
         return 0
-
 
 if __name__ == '__main__':
     pass
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,17 +10,30 @@ def top_ten(subreddit):
     """Get the titles of the top ten post list for a give subreddit
     """
     headers = {'User-Agent': 'ALX Client'}
-    time_filter = 'day'
     limit = 10
-    url = 'https://www.reddit.com/r/{}/top.json?t={}&limit={}'.format(
+    url = "https://www.reddit.com/r/{}/top.json?limit={}".format(
         subreddit,
-        time_filter,
         limit
     )
+    
     response = requests.get(url, headers=headers, allow_redirects=False)
+    
     if response.status_code == 200:
-        for post in response.json().get('data').get('children'):
-            print(post.get('data').get('title'))
+        try:
+            data = response.json().get('data', {})
+            posts = data.get('children',[])
+            
+            if not posts:
+                print(None)
+                return
+
+            for post in posts:
+                print(post.get('data').get('title'))
+
+        except(ValueError, KeyError):
+            print(None)
+    elif response.status_code == 404:
+        print('None')
     else:
         print('None')
 
