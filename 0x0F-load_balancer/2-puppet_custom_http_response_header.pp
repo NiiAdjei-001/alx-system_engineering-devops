81db564f7b65d73957b027437f5f30a681664c4d
Nii Adjei Osae <niiadjeiosae.technology@gmail.com>
2024-08-29 13:45:04 +0000

    Fixed custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,74 +1,72 @@
-# Install and Configure Nginx
-package { 'nginx':
-  ensure => installed,
+# Install and Configure Nginx Web Server
+$package_name = 'nginx'
+$service_name = 'nginx'
+$webserver_config_file = '/etc/nginx/sites-available/default'
+$config_template = "#Default Server Configuration
+server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
+
+	root /var/www/html;
+
+	index index.html index.htm index.nginx-debian.html;
+
+	add_header X-served-By \$hostname;
+
+	server_name _;
+
+	location = / {
+		try_files \$uri \$uri/ =200;
+	}
+  
+  error_page 301 /custom_301.html;
+	location = /redirect_me {
+		return 301 /custom_301.html;
+	}
+
+	error_page 404 /custom_404.html; 
+	location /custom_404.html {
+		internal;
+	}
+}
+"
+
+package { $package_name:
+	ensure => installed,
 }
 
-service { 'nginx':
-  ensure  => true,
-  enable  => true,
-  require => Package['nginx'],
+service { $service_name:
+	ensure     => running,
+	enable     => true,
+	hasrestart => true,
+	require    => Package[$package_name],
+}
+
+file { $webserver_config_file:
+	ensure  => present,
+	content => $config_template,
+	require => Package[$package_name],
+	notify  => Service[$service_name],
 }
 
 file { '/var/www/html':
-  ensure  => present,
-  require => Package['nginx'],
+	ensure => 'directory',
 }
 
 file { '/var/www/html/index.html':
-  ensure  => present,
-  content => 'Hello World!',
-  require => Package['nginx'],
+	ensure  => 'file',
+	content => 'Hello World!',
+	require => File['/var/www/html']
 }
 
-file { '/var/www/html/404.html':
-  ensure  => present,
-  content => "Ceci n'est pas une page",
-  require => Package['nginx'],
+file { '/var/www/html/custom_301.html':
+	ensure  => 'file',
+	content => '301 Moved Permanently', 
+	require => File['/var/www/html'],
 }
 
-file { '/var/www/html/redirect_me.html':
-  ensure  => present,
-  content => 'Moved Permanently',
-  require => Package['nginx'],
-}
-
-file { '/etc/nginx/nginx.conf':
-  ensure  => present,
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-file { '/etc/nginx/sites-available/default':
-  ensure  => present,
-  content => "
-#Default Server Configuration
-server {
-  listen 80 default_server;
-  listen [::]:80 default_server;
-
-  root /var/www/html;
-
-  index index.html index.htm index.nginx-debian.html;
-
-  add_header X-Served-By \$hostname;
-
-  server_name _;
-
-  location /redirect_me {
-    return 301 /redirect_me;
-  }
-
-  error_page 404 /404.html;
- 
-  location = /404.html {
-    internal;
-  }
-
-  location = / {
-    try_files \$uri \$uri/ =404;
-  }
-}
-",
-  require => Package['nginx'],
-  notify  => Service['nginx'],
+file { '/var/www/html/custom_404.html':
+	ensure  => 'file',
+	content => "Ceci n'est pas une page",
+	require => File['/var/www/html'],
 }
